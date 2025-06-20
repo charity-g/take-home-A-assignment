54f3814c195d2e7eff85ee3a6f4172af8959df8d
cherr <charity.grey1@gmail.com>
2025-06-20 00:49:15 -0700

    add mantine table

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,12 @@ import DataTable from "@/src/data-table";
 
 export default function Home() {
   return (
-    <div className='min-h-screen'> hello world
+    <div className='min-h-screen'>
+    <div className="container mx-auto p-6">
+      
       <DataTable />
+
+    </div>
     </div>
   );
 }
--- frontend/src/data-table.tsx
+++ frontend/src/data-table.tsx
@@ -1,13 +1,26 @@
-"use client"
-
-
-
 export default function DataTable() {
 
     return (
-        <div className='min-h-screen'>
-            Data Table Component
-        </div> 
+         <div
+          className='rounded-lg border overflow-hidden'>
+            <table className="w-full">
+                <thead>
+              <tr className="bg-gray-700 border-gray-600 border-b">
+                <th className="text-left py-4 px-6 font-medium text-gray-300">
+                  Table Header
+                </th>
+                <th className="text-left py-4 px-6 font-medium text-gray-300">
+                  Table Header
+                </th>
+                <th className="text-left py-4 px-6 font-medium text-gray-300">
+                  Table Header
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+            </table>
+          </div>
     )
 
 }
