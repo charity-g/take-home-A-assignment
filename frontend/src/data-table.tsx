337c41fc2200ea0a7badfc93039aed06f09b891c
cherr <charity.grey1@gmail.com>
2025-06-20 20:56:13 -0700

    add mantine ui and set up data table

--- frontend/src/data-table.tsx
+++ frontend/src/data-table.tsx
@@ -1,26 +1,53 @@
+'use client'
+
+import { useMemo, useState } from 'react'
+import import_data from '../form_data.json'
+import { FormData } from './types'
+import {
+  MantineReactTable,
+  useMantineReactTable,
+  type MRT_ColumnDef,
+} from 'mantine-react-table'
+import { useMantineTheme } from '@mantine/core'
+
 export default function DataTable() {
+  const { colorScheme } = useMantineTheme();
+  const [data, setData] = useState<FormData[]>(import_data as FormData[])
 
-    return (
-         <div
-          className='rounded-lg border overflow-hidden'>
-            <table className="w-full">
-                <thead>
-              <tr className="bg-gray-700 border-gray-600 border-b">
-                <th className="text-left py-4 px-6 font-medium text-gray-300">
-                  Table Header
-                </th>
-                <th className="text-left py-4 px-6 font-medium text-gray-300">
-                  Table Header
-                </th>
-                <th className="text-left py-4 px-6 font-medium text-gray-300">
-                  Table Header
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-            </table>
-          </div>
-    )
+  const columns = useMemo<MRT_ColumnDef<FormData>[]>(
+    () => [
+      {
+        accessorKey: 'question',
+        header: 'Question',
+      },
+      {
+        accessorKey: 'answer',
+        header: 'Answer',
+      },
+    ],
+    []
+  )
 
+  const table = useMantineReactTable({
+    columns,
+    data,
+    mantineTableProps: {
+      highlightOnHover: false,
+      withColumnBorders: true,
+      withBorder: colorScheme === 'dark',
+      sx: {
+        'thead > tr': { backgroundColor: 'inherit' },
+        'thead > tr > th': { backgroundColor: 'inherit' },
+        'tbody > tr > td': { backgroundColor: 'inherit' },
+      },
+    },
+  })
+
+  return (
+    <div className="min-h-screen">
+      <div className="container mx-auto p-6">
+        <MantineReactTable table={table} />
+      </div>
+    </div>
+  )
 }
